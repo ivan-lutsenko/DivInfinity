67f8f71d54418b076e9572c6928cdc7b48da0ad8
ivan-lutsenko <boss.ivan55@mail.ru>
2018-03-21 14:53:56 +0300

    Adaptive design
    design

--- js/script-fixed.js
+++ js/script-fixed.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 300) 
-			$(".substrate").addClass("position");
-		else
-			$(".substrate").removeClass("position");
-	});
+	if ($(window).width() > 767)
+	{
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 300) 
+				$(".substrate").addClass("position");
+			else
+				$(".substrate").removeClass("position");
+		});
+	}
 });
--- js/script-menu.js
+++ js/script-menu.js
@@ -1,14 +1,42 @@
 $(document).ready(function(){
-	setTimeout(function(){
-		$(".menu nav ul li:nth-child(2)").addClass("slideInDown animated opacity");
-	}, 500);
-	setTimeout(function(){
-		$(".menu nav ul li:nth-child(3)").addClass("slideInDown animated opacity");
-	}, 1000);
-	setTimeout(function(){
-		$(".menu nav ul li:nth-child(4)").addClass("slideInDown animated opacity");
-	}, 1500);
-	setTimeout(function(){
-		$(".menu nav ul li:nth-child(5)").addClass("slideInDown animated opacity");
-	}, 2000);
+	//Note osob
+	$(".menu-1").click(function(){
+		//animation hamburger (open)
+		$(".menu-1").toggleClass("trans");
+		$("body").toggleClass("overflow");
+		$(".menu").removeClass("fadeInDown animated");
+		$(".menu").addClass("show fadeInLeft animated");
+
+		setTimeout(function(){
+			$(".menu nav ul li:nth-child(1)").addClass("slideInDown animated opacity");
+		}, 200);
+		setTimeout(function(){
+			$(".menu nav ul li:nth-child(2)").addClass("slideInDown animated opacity");
+		}, 400);
+		setTimeout(function(){
+			$(".menu nav ul li:nth-child(3)").addClass("slideInDown animated opacity");
+		}, 600);
+		setTimeout(function(){
+			$(".menu nav ul li:nth-child(4)").addClass("slideInDown animated opacity");
+		}, 800);
+		setTimeout(function(){
+			$(".menu nav ul li:nth-child(5)").addClass("slideInDown animated opacity");
+		}, 1000);
+	});
+	$(".cross-1").click(function(){
+		//animation hamburger
+		$(".cross-1").toggleClass("trans");
+		$("body").toggleClass("overflow");
+		$(".menu").removeClass("fadeInLeft");
+		$(".menu").addClass("fadeOutLeft");
+		setTimeout(function(){
+			$(".menu").removeClass("show fadeOutLeft animated");
+
+			$(".menu nav ul li:nth-child(1)").removeClass("slideInDown animated opacity");
+			$(".menu nav ul li:nth-child(2)").removeClass("slideInDown animated opacity");
+			$(".menu nav ul li:nth-child(3)").removeClass("slideInDown animated opacity");
+			$(".menu nav ul li:nth-child(4)").removeClass("slideInDown animated opacity");
+			$(".menu nav ul li:nth-child(5)").removeClass("slideInDown animated opacity");
+		}, 500);
+	});
 });
