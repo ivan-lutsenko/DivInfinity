d88b0d8d25c20539f29ce0721b5002569c24f3e8
ivan-lutsenko <boss.ivan55@mail.ru>
2018-04-12 03:47:28 +0300

    Production
    Production

--- js/script-mask.js
+++ js/script-mask.js
@@ -1,5 +1,21 @@
-$("#phone").mask("+375(99) 999-99-99");
-$('#mail').inputmask({
+$("#phone-1").mask("+375(99) 999-99-99");
+$('#mail-1').inputmask({
+	mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+	greedy: false,
+	onBeforePaste: function (pastedValue, opts) {
+		pastedValue = pastedValue.toLowerCase();
+		return pastedValue.replace("mailto:", "");
+	},
+	definitions: {
+		'*': {
+			validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+			cardinality: 1,
+			casing: "lower"
+		}
+	}
+});
+$("#phone-2").mask("+375(99) 999-99-99");
+$('#mail-2').inputmask({
 	mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
 	greedy: false,
 	onBeforePaste: function (pastedValue, opts) {
